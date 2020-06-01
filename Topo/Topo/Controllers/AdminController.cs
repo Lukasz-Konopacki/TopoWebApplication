a5b79b70d26e7dfc7a0d81d3aa389a4159046c0f
Lukasz Konopacki <luk.konopacki@gmail.com>
2020-06-01 21:43:58 +0200

    Dodano panel admina

---
+++ Topo/Topo/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Topo.Model;
+
+namespace Topo.Controllers
+{
+    public class AdminController : Controller
+    {
+        public readonly EFCContext Context;
+
+        public AdminController(EFCContext context)
+        {
+            Context = context;
+        }
+
+
+        public IActionResult Panel()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddRegion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddRegion(Region model)
+        {
+            return Content($"{model.Name}\n" +
+                $"{model.Description}\n" +
+                $"{model.PostionLat}\n" +
+                $"{model.PostionLat}");
+        }
+
+
+    }
+}
